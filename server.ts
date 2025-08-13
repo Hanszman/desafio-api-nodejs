8436fe90f979126871f9f61e5049de557ba6c582
Hanszman <victor.hanszman@hotmail.com>
2025-08-13 17:59:53 -0300

    keep updating with second class

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import fastify from 'fastify';
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
 import { db } from './src/database/client.ts'
 import { eq } from 'drizzle-orm'
 import { courses } from './src/database/schema.ts';
@@ -13,7 +15,10 @@ const server = fastify({
             },
         },
     }
-});
+}).withTypeProvider<ZodTypeProvider>();
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
 
 server.get('/courses', async (request, response) => {
     const result = await db.select({
@@ -39,15 +44,14 @@ server.get('/courses/:id', async (request, response) => {
     return response.status(404).send();
 });
 
-server.post('/courses', async (request, response) => {
-    type Body = {
-        title: string
+server.post('/courses', {
+    schema: {
+        body: z.object({
+            title: z.string().min(5, 'Title must have at least 5 characters.'),
+        }),
     }
-    const body = request.body as Body;
-    const courseTitle = body.title;
-    if (!courseTitle) {
-        return response.status(400).send({ message: 'Title is required.' });
-    }
+}, async (request, response) => {
+    const courseTitle = request.body.title;
     const result = await db
         .insert(courses)
         .values({ title: courseTitle })
