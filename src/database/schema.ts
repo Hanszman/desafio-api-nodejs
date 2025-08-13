876d4790aa9c3b335651ae0526c924f155b0b5ff
Hanszman <victor.hanszman@hotmail.com>
2025-08-13 20:20:51 -0300

    tried to keep up until almost 1h of the third class

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,5 +1,4 @@
-import { pgTable, uuid, text } from 'drizzle-orm/pg-core'
-import { timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
     id: uuid().primaryKey().defaultRandom(),
@@ -18,4 +17,6 @@ export const enrollments = pgTable('enrollments', {
     userId: uuid().notNull().references(() => users.id),
     courseId: uuid().notNull().references(() => courses.id),
     createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+// }, table => {
+//     uniqueIndex().on(table.userId, table.courseId)
 });
